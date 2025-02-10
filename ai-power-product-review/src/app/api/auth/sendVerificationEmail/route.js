5c812833853d92b7ec140b85910e86df9b222143
Empire688682 <asehindej@gmail.com>
2025-02-10 07:56:51 +0100

    user

--- ai-power-product-review/src/app/api/auth/sendVerificationEmail/route.js
+++ ai-power-product-review/src/app/api/auth/sendVerificationEmail/route.js
@@ -7,6 +7,7 @@ export const sendVerificationEmail = async (toEmail, verificationLink) => {
         const transporter = nodemailer.createTransport({
             host:process.env.EMAIL_HOST,
             port:Number(process.env.EMAIL_PORT),
+            secure:true,
             auth: {
                 user:process.env.EMAIL_USER,
                 pass:process.env.EMAIL_PASS,
