774086817d0ee2a9da721db3c7fe53f80fc84638
Empire688682 <asehindej@gmail.com>
2025-02-13 13:20:50 +0100

    VerifyEmailWrapper

--- ai-power-product-review/src/app/verify-email/page.jsx
+++ ai-power-product-review/src/app/verify-email/page.jsx
@@ -1,54 +1,13 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import axios from "axios";
-import LoadingSpinner from "@/Component/LoadingSpinner/LoadingSpinner";
-import styles from "./VerifyEmail.module.css";
+import VerifyEmailWrapper from '@/Component/VerifyEmailWrapper/VerifyEmailWrapper';
+import React from 'react'
 
 const Page = () => {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const username = searchParams.get("username");
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const verifyEmail = async () => {
-      try {
-        const response = await axios.post("/api/auth/verifyemail", { token });
-        if (response.data.success) {
-          const userData = response.data.data;
-          localStorage.setItem("userData", JSON.stringify(userData));
-          setMessage("Email verified successfully! Redirecting...");
-          setTimeout(() => {
-            router.push("/userAnalysis");
-          }, 2000);
-        } else {
-          setMessage("Verification failed. Please try again.");
-        }
-      } catch (error) {
-        setMessage("An error occurred during verification. Please try again.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    verifyEmail();
-  }, [token, router]);
-
   return (
-    <div className={styles.verifyEmail}>
-      {loading ? (
-        <div className={styles.loadingContainer}>
-          <LoadingSpinner />
-          <p>Verifying {username}'s email...</p>
-        </div>
-      ) : (
-        <h1>{message}</h1>
-      )}
+    <div>
+      <VerifyEmailWrapper />
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
